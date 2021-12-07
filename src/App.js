2b819d0200f8006dadada829e0c4cc084062685b
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-12-07 13:59:43 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import { Fragment } from "react";
 import NavBar from "./components/section/Navbar";
-
+import {ThemeProvider, createTheme} from '@material-ui/core/styles'
 
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#4a148c',
+      },
+      secondary: {
+        main: '#ab47bc',
+      },
+    },
+  });
   return (
     <Fragment>
+      <ThemeProvider theme={theme}>
       <NavBar />
+      </ThemeProvider>
     </Fragment>
   );
 }
