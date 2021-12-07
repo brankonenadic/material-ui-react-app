336f3815086288e58d64e1bc884b26f4188f1584
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-12-07 14:41:34 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Fragment } from "react";
 import NavBar from "./components/section/Navbar";
 import {ThemeProvider, createTheme} from '@material-ui/core/styles'
+import { Button } from "@material-ui/core";
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
     <Fragment>
       <ThemeProvider theme={theme}>
       <NavBar />
+      <Button size="large" color="secondary" variant="contained">Secondary</Button>
       </ThemeProvider>
     </Fragment>
   );
