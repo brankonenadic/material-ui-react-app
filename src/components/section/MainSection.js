a9a3ccd6ecabe04fd1b661bb4a647d84c53516a5
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-12-10 14:41:20 +0100

    Update

--- src/components/section/MainSection.js
+++ src/components/section/MainSection.js
@@ -18,6 +18,31 @@ const MainSection = () => {
       To create a production bundle, use `npm run build` or `yarn build
                     </Typography>
                 </Paper>
+                 <Paper>
+                    <Typography>
+                        This is paper !
+                    This HTML file is a template.
+      If you open it directly in the browser, you will see an empty page.
+
+      You can add webfonts, meta tags, or analytics to this file.
+      The build step will place the bundled scripts into the tag.
+
+      To begin the development, run `npm start` or `yarn start`.
+      To create a production bundle, use `npm run build` or `yarn build
+                    </Typography>
+                </Paper> <Paper>
+                    <Typography>
+                        This is paper !
+                    This HTML file is a template.
+      If you open it directly in the browser, you will see an empty page.
+
+      You can add webfonts, meta tags, or analytics to this file.
+      The build step will place the bundled scripts into the tag.
+
+      To begin the development, run `npm start` or `yarn start`.
+      To create a production bundle, use `npm run build` or `yarn build
+                    </Typography>
+                </Paper>
             </Container>
         </section>
     )
