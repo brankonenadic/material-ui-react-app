fa21c59f6f9e88f152f78bbd9b2ace15f372b6b3
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-12-11 22:57:05 +0100

    Update

--- src/components/section/MainSection.js
+++ src/components/section/MainSection.js
@@ -5,42 +5,43 @@ const MainSection = () => {
     return (
         <section>
             <Container>
-                <Paper>
+                <Paper variant="outlined">
                     <Typography>
                         This is paper !
-                    This HTML file is a template.
-      If you open it directly in the browser, you will see an empty page.
+                        This HTML file is a template.
+                        If you open it directly in the browser, you will see an empty page.
 
-      You can add webfonts, meta tags, or analytics to this file.
-      The build step will place the bundled scripts into the tag.
+                        You can add webfonts, meta tags, or analytics to this file.
+                        The build step will place the bundled scripts into the tag.
 
-      To begin the development, run `npm start` or `yarn start`.
-      To create a production bundle, use `npm run build` or `yarn build
+                        To begin the development, run `npm start` or `yarn start`.
+                        To create a production bundle, use `npm run build` or `yarn build
                     </Typography>
                 </Paper>
-                 <Paper>
+                <Paper variant="outlined">
                     <Typography>
                         This is paper !
-                    This HTML file is a template.
-      If you open it directly in the browser, you will see an empty page.
+                        This HTML file is a template.
+                        If you open it directly in the browser, you will see an empty page.
 
-      You can add webfonts, meta tags, or analytics to this file.
-      The build step will place the bundled scripts into the tag.
+                        You can add webfonts, meta tags, or analytics to this file.
+                        The build step will place the bundled scripts into the tag.
 
-      To begin the development, run `npm start` or `yarn start`.
-      To create a production bundle, use `npm run build` or `yarn build
+                        To begin the development, run `npm start` or `yarn start`.
+                        To create a production bundle, use `npm run build` or `yarn build
                     </Typography>
-                </Paper> <Paper>
+                </Paper>
+                <Paper variant="outlined">
                     <Typography>
                         This is paper !
-                    This HTML file is a template.
-      If you open it directly in the browser, you will see an empty page.
+                        This HTML file is a template.
+                        If you open it directly in the browser, you will see an empty page.
 
-      You can add webfonts, meta tags, or analytics to this file.
-      The build step will place the bundled scripts into the tag.
+                        You can add webfonts, meta tags, or analytics to this file.
+                        The build step will place the bundled scripts into the tag.
 
-      To begin the development, run `npm start` or `yarn start`.
-      To create a production bundle, use `npm run build` or `yarn build
+                        To begin the development, run `npm start` or `yarn start`.
+                        To create a production bundle, use `npm run build` or `yarn build
                     </Typography>
                 </Paper>
             </Container>
